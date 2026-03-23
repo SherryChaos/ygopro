6fb8398a9c3e05c47faf0a57b981e29561ca689c
SherryChaos <60814455+SherryChaos@users.noreply.github.com>
2026-03-23 16:50:40 +0800

    Update deck_manager.cpp

--- gframe/deck_manager.cpp
+++ gframe/deck_manager.cpp
@@ -54,7 +54,7 @@ void DeckManager::LoadLFList() {
 	LoadLFListSingle("config/lflist.conf");
 #endif
 #ifdef SERVER_PRO3_SUPPORT
-	LoadLFListSingle("Data/lflist.conf");
+	LoadLFListSingle("Data/lflist_merged.conf");
 #endif
 	LoadLFListSingle("expansions/lflist.conf");
 	LoadLFListSingle("lflist.conf");
